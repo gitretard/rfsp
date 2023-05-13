c06537f952c409b37a82654c5714433712a2c8e6
gitretard <znoobbitcatspro@gmail.com>
2023-05-13 13:11:39 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# RFSP (Retarded file sending protocol) standard
+
+# TODO! Implement actual file trasnfer
+Current state: Metadata works.
+Please note that ive never made a fucking protocol before.
+
+
+Metadata packet (512 bytes):
+The metadata packet will be sent to the receiver at the start of sending the file.
+0-7 bytes: u64 size in bytes
+8-512 bytes: Filename (will be truncated if longer than that) // Kept at 1 packet for the sake of simplicity
+
+
+Data packet (512 bytes):
+0-7 bytes: The Hash of the entire packet (u64 rust default hasher) 
+8 byte: Opcode (1:Cancel,2:Finished: do final op) // More opcodes will be added
+9-17 bytes: Packet count that starts as 0. so if the same packet gets sent again ignore the packet (u32)  (Thanks tom scott)
+18-512 bytes: The actual file
+
+For each packet the receiver will send the following back to the sender
+1: Everything is okay. Continue as normal, 2: Resend (Err || Mismatched hash). 3: Cancelled by user (ctrl+C)
+
+If the hash of the received packet isnt the same as the first 8 bytes then the receiver will will send 1 to sender and the sender will resend the packet again if it still isnt the same for the 3rd time the operation will be cancelled
+
+If the opcode is 1. The receiver will close the connection and delete the downloaded file
+
+
+If the opcode is 2 then
+the sender will send the hash of the file and the receiver must check the hash of the file if the hash of the file is incorrect then resend it 1 more time 
+if the file still doesnt have the correct hash. the operation is cancelled by the sender by saying whatever the sender likes (128 bytes limit) then closing the connection
+
+Actual file sent in each packet = 494
+
+All of these packets will be in a u8 array with the length of 512
---
+++ src/main.rs
@@ -0,0 +1,123 @@
+mod librecv;
+mod libsend;
+use std::{
+    env,
+    io::{Read, Write},
+    net::{self, TcpStream},
+    str, time,
+};
+
+const RW_TIMEOUT: u64 = 30;
+const MAX_RETRIES: u32 = 3;
+
+fn send(f: &str) {
+    let mut conn = match net::TcpStream::connect("127.0.0.1:5333") {
+        Ok(r) => r,
+        Err(a) => {
+            println!("Error in tcpcon: {}", a);
+            return;
+        }
+    };
+    conn.set_read_timeout(Some(time::Duration::from_secs(RW_TIMEOUT)))
+        .unwrap(); // Not doing these
+    conn.set_write_timeout(Some(time::Duration::from_secs(RW_TIMEOUT)))
+        .unwrap();
+    let meta = libsend::craft_metadata(f);
+    println!("{:?}", meta);
+    for _ in 0..MAX_RETRIES {
+        match conn.write_all(&meta) {
+            Ok(_) => {}
+            Err(s) => {
+                println!("Send first packet failed: {}", s);
+                continue;
+            }
+        }
+        conn.flush().unwrap(); // no i am not making a match statement for this one
+                               // Confirm if data has reached
+        let mut buf: [u8; 2] = [0; 2];
+        for _ in 0..3 {
+            let bytes_read: usize = match conn.read(&mut buf) {
+                Err(s) => {
+                    println!(
+                        "Failed to get confirmation (cant read) (Metadata): {s}\nTrying again..."
+                    );
+                    continue;
+                }
+                Ok(s) => s,
+            };
+            if bytes_read == 0 {
+                continue;
+            }
+        }
+        if buf[0] < 1 || buf[0] > 3 {
+            println!("Invalid status code! The reciever probably isn't a RFSP receiver... Closing the connection (no retries)");
+            return;
+        }
+        match buf[0] {
+            // Dont do anything except print yet
+            1 => {
+                println!("Metadata status packet: 1");
+                break;
+            }
+            2 => {
+                println!("Metadata status packet: 2... Retrying");
+                continue;
+            }
+            _ => {}
+        }
+    }
+}
+
+fn recv() {
+    let listener = match net::TcpListener::bind("127.0.0.1:5333") {
+        Ok(a) => a,
+        Err(p) => {
+            println!("Error in binding Tcpln: {}", p);
+            return;
+        }
+    };
+    let mut filen = "";
+    let mut filesize = 0;
+    for conn in listener.incoming() {
+        let mut conn = conn.unwrap(); //gee
+        conn.set_read_timeout(Some(time::Duration::from_secs(RW_TIMEOUT))).unwrap();
+        conn.set_write_timeout(Some(time::Duration::from_secs(RW_TIMEOUT))).unwrap();
+        let mut buf: [u8; 512] = [0; 512];
+        // read
+        match conn.read(&mut buf) {
+            Ok(_) => {}
+            Err(s) => {
+                println!("Error in TcpRead (first packet): {s}. Cannnot continue");
+                return;
+            }
+        }
+        // IF i have problems. its because how i index
+        // Wont do anything with opcode yet
+        let arr: [u8; 8] = buf[0..8]
+            .try_into()
+            .expect("Error in converting [u8] into [u8;8]");
+        let filesize: u64 = u64::from_be_bytes(arr);
+        filen = match str::from_utf8(&buf[8..512]) {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Failed to conver [u8] into &str {e}");
+                return;
+            } // maybe return
+        };
+        println!("filesize: {filesize}\nfilen: {filen}");
+        conn.write(&[1]).unwrap();
+        break;
+    }
+}
+
+fn main() {
+    let a = env::args().nth(3); // cargo run . -- ?
+    if let Some(s) = a {
+        println!("{s}");
+        match s.as_str() {
+            "send" => send("src/main.rs"),
+            "recv" => recv(),
+            _ => {}
+        }
+    }
+}
